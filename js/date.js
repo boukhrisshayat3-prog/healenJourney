154611874d307e9f603a1bd90fddfb2d736dc163
boukhrisshayat3-prog <boukhrisshayat3@alumnos.cei.es>
2026-04-25 04:56:26 +0100

    actualizar

--- js/date.js
+++ js/date.js
@@ -1,7 +1,7 @@
 export const datos = [
   {
     id: 1,
-    imagen: "./assets/img/img-slider1.png",
+    imagen: "./assets/img/img-slider1.jpg",
     titulo: "Connect with Others",
     parrafo: "Join a supportive community where you can share experiences and receive understanding from people on a similar journey.",
     botonTexto: "Join Now",
@@ -9,7 +9,7 @@ export const datos = [
   },
   {
     id: 2,
-    imagen: "./assets/img/img-slider2.png",
+    imagen: "./assets/img/img-slider2.jpg",
     titulo: "Supportive Community",
     parrafo: "Meet people who understand your journey and share experiences in a safe and supportive environment.",
     botonTexto: "Register",
@@ -17,7 +17,7 @@ export const datos = [
   },
   {
     id: 4,
-    imagen: "./assets/img/img-slider4.png",
+    imagen: "./assets/img/img-slider4.jpg",
     subtitulo: "Expert guidance to support your mental health",
     titulo: "Expert Guidance",
     parrafo: "Access resources and guidance created by mental health professionals to help you feel supported.",
@@ -26,7 +26,7 @@ export const datos = [
   },
   {
     id: 5,
-    imagen: "./assets/img/img-slider5.png",
+    imagen: "./assets/img/img-slider5.jpg",
     titulo: "You Have a Family Here",
     parrafo: "You are not alone. Share your problems with people who care and are ready to support you.",
     botonTexto: "ver todo",
@@ -34,7 +34,7 @@ export const datos = [
   },
   {
     id: 6,
-    imagen: "./assets/img/img-slider6.png",
+    imagen: "./assets/img/img-slider6.jpg",
     titulo: "Support Groups",
     parrafo: "Find a safe place to share, heal, and connect through expert and peer-led sessions.",
     botonTexto: "View All",
@@ -42,7 +42,7 @@ export const datos = [
   },
   {
     id: 7,
-    imagen: "./assets/img/img-slider7.png",
+    imagen: "./assets/img/img-slider7.jpg",
     titulo: "You Are Not Alone",
     parrafo: "Whenever you feel overwhelmed, remember our team is here to listen and support you 24/7.",
     botonTexto: "Contact Us Now",
